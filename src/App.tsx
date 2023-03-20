f7380034f0c0323665429da5d491f44a184567ae
Jardel Belisario <jardelbelisario.hvar@suzano.com.br>
2023-03-20 14:41:13 -0300

    feat: add setup project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+         <h1>OLA MUNDO</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
