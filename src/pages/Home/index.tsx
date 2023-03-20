dc1b9edda3be181719ff9cad187492d5a44bbda5
Jardel Belisario <jardelbelisario.hvar@suzano.com.br>
2023-03-20 17:51:37 -0300

    feat: home list characters

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,69 @@
 import { useEffect, useState } from "react";
 import api from "../../service/axios";
-import { HomeContainer, HomeContent } from "./style";
+import { CardCharacter, CardCharacterBackground, CardContent, HomeContainer, HomeContent, HomeTitle, Pagination, PaginationItem } from "./style";
 
 
 export default function Home() {
 
 
-  const [dada, setData] = useState();
+  const [characters, setCharacters] = useState<any>({});
 
   const getComics = async () => {
-    const response = await api.get("/v1/public/comics")
-    setData(response.data)
-    console.log(response.data)
+
+    try {
+          const response = await api.get("/v1/public/characters")
+          setCharacters(response.data.data.results)
+          console.log(response.data)
+          console.log(response.data.data.results)
+      
+    } catch (error) { 
+      console.log(error)
+      
+    }
   }
 
   useEffect(() => {
-
     getComics()
-
   },[])
 
   return (
     <HomeContainer>
+
       <HomeContent>
 
-      <h1>HOME PAGE</h1>
+       <HomeTitle>Listagem de quadrinhos</HomeTitle>
+
+
+    	  {characters.length > 0 && (
+
+          <CardContent>
+
+            {characters.map((character: any, key: number) =>(
+              <CardCharacter key={key} >
+                <CardCharacterBackground bgCardCharacter={`${character.thumbnail.path}.${character.thumbnail.extension}`}>
+
+                
+                </CardCharacterBackground>
+                {/* <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt="" /> */}
+              </CardCharacter>
+            ))}
+            
+        </CardContent>
+        
+        )}
+
+        <Pagination>
+          <PaginationItem bgPagintationActive={true}>1</PaginationItem>
+          <PaginationItem>2</PaginationItem>
+          <PaginationItem>3</PaginationItem>
+          <PaginationItem>4</PaginationItem>
+          <PaginationItem>5</PaginationItem>
+          <PaginationItem>6</PaginationItem>
+          <PaginationItem>7</PaginationItem>
+          <PaginationItem>8</PaginationItem>
+        </Pagination>
+            
+
       </HomeContent>
     </HomeContainer>
   )
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,19 +1,106 @@
 import styled from "styled-components";
 
+type PropsBgCardCharacter = {
+  bgCardCharacter: string;
+};
+
+type PropsPaginationItem= {
+  bgPagintationActive?: boolean;
+};
+
 export const HomeContainer = styled.div`
-  background: ${props => props.theme['gray-900']} 
+  background: ${(props) => props.theme["gray-900"]} 
   padding: 2.5rem 0 7.5rem;
   width: 100%;
 
-`
+`;
+
 export const HomeContent = styled.div`
   width: 100%;
   max-width: 1120px;
   height: 100%;
-  margin: 5rem auto; 
-  padding: 0 1.5rem ;
+  margin: 5rem auto;
+  padding: 0 1.5rem;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HomeTitle = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  color: ${(props) => props.theme["gray-100"]};
+`;
+
+export const CardContent = styled.div`
+  margin: 4rem auto;
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
+  flex-wrap: wrap;
+  gap: 4;
+`;
+
+export const CardCharacter = styled.div`
+  width: 21rem;
+  margin-bottom: 2rem;
+  height: 17.5rem;
+  /* background-color: ${(props) => props.theme["gray-300"]}; */
+  border-radius: 10px;
+
+  width: 300px;
+  height: 200px;
+  border: 1px solid #000000;
+  overflow: hidden;
+  position: relative;
+`;
+
+
+export const CardCharacterBackground = styled.div<PropsBgCardCharacter>`
+  width: 100%;
+  height: 100%;
+  background: url(${(props) =>
+    props.bgCardCharacter ?? props.theme["gray-300"]});
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: absolute;
+  transition: 0.5s;
+  cursor: pointer;
+  :hover {
+    transform: scale(1.2);
+  }
+`;
+
+export const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4;
+
+  width: 100%;
+`;
+
+
+export const PaginationItem = styled.div<PropsPaginationItem>`
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: ${props => props.bgPagintationActive ? 
+  (props) => props.theme["green-500"] : (props) => props.theme["gray-500"]};
+  margin: 0 5px;
+
+ 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  cursor: pointer;
+
+  :hover{
+    background-color: ${(props) => props.theme["green-300"]};
+  }
+  
+  
+`;
