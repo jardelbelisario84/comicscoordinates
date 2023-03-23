05caf5a42bfaa8dbe6657ff4f773c6209ee809f1
Jardel Belisario <jardelbelisario.hvar@suzano.com.br>
2023-03-23 11:24:39 -0300

    refactor: add page ensure basename 404 v3

--- src/utils/ensure-basename.js
+++ src/utils/ensure-basename.js
@@ -1,7 +1,28 @@
-if (!window.location.pathname.includes(document.baseURI.substring(document.baseURI.indexOf(window.location.origin) + window.location.origin.length, document.baseURI.lastIndexOf('/')))) {
-  window.history.replaceState(
-    '',
-    '',
-    document.baseURI.substring(document.baseURI.indexOf(window.location.origin) + window.location.origin.length, document.baseURI.lastIndexOf('/')) + window.location.pathname
-  );
-}
+// if (
+//   !window.location.pathname.includes(
+//     document.baseURI.substring(
+//       document.baseURI.indexOf(window.location.origin) + window.location.origin.length,
+//       document.baseURI.lastIndexOf("/")
+//     )
+//   )
+// ) {
+//   window.history.replaceState(
+//     "",
+//     "",
+//     document.baseURI.substring(
+//       document.baseURI.indexOf(window.location.origin) + window.location.origin.length,
+//       document.baseURI.lastIndexOf("/")
+//     ) + window.location.pathname
+//   );
+// }
+
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
