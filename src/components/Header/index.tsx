97c251400f4ece3341fc9fa635e30b835f61797a
Jardel Belisario <jardelbelisario.hvar@suzano.com.br>
2023-03-24 01:02:48 -0300

    refactor: update close link menu responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -38,11 +38,11 @@ export function Header() {
       <HeaderContentMobile visible={menuMobile}>
 
         <HeaderMenuMobile>
-          <NavLink to="/">Início</NavLink>
-          <NavLink to="/comics">Quadrinhos</NavLink>
-          <NavLink to="/characters">Personagens</NavLink>
-          <NavLink to="/maps">Mapa</NavLink>
-          <NavLink to="/address">Histórico de endereços</NavLink>
+          <NavLink to="/" onClick={handleMenuMobile}>Início</NavLink>
+          <NavLink to="/comics" onClick={handleMenuMobile}>Quadrinhos</NavLink>
+          <NavLink to="/characters" onClick={handleMenuMobile}>Personagens</NavLink>
+          <NavLink to="/maps" onClick={handleMenuMobile}>Mapa</NavLink>
+          <NavLink to="/address" onClick={handleMenuMobile}>Histórico de endereços</NavLink>
         </HeaderMenuMobile>
 
       </HeaderContentMobile>
